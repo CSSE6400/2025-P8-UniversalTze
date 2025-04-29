4b8e55f2908ad7e17826b00ef455ec4ec0389e8d
Tze Kheng Goh <dgoh03625@yahoo.com>
2025-04-29 19:49:14 +1000

    Added logger formatting and one for beginning and of request

--- todo/__init__.py
+++ todo/__init__.py
@@ -1,28 +1,38 @@
 import os
 import boto3
+import uuid 
 import watchtower, logging
-from flask import Flask
+from flask import Flask, has_request_context, request
 from flask_sqlalchemy import SQLAlchemy 
 
 def create_app(config_overrides=None): 
    logging.basicConfig(level=logging.INFO)
 
-   app = Flask(__name__, static_folder='app', static_url_path="/") 
+   app = Flask(__name__, static_folder='app', static_url_path="/")
  
    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLALCHEMY_DATABASE_URI", "sqlite:///db.sqlite")
    if config_overrides: 
        app.config.update(config_overrides)
+
+   @app.before_request 
+   def before_request(): 
+      request.environ['REQUEST_ID'] = str(uuid.uuid4()) 
+      requests.info("Request started") 
  
-   handler = watchtower.CloudWatchLogHandler(
+   handler = watchtower.CloudWatchLogHandler(   # Configures watchtower to log to AWS CloudWatch.
            log_group_name="taskoverflow",
            boto3_client=boto3.client("logs", region_name="us-east-1")
    )
+   handler.setFormatter(StructuredFormatter())
    app.logger.addHandler(handler)
    logging.getLogger().addHandler(handler)
    logging.getLogger('werkzeug').addHandler(handler)
    logging.getLogger("sqlalchemy.engine").addHandler(handler)
    logging.getLogger("sqlalchemy.engine").setLevel(logging.INFO)
 
+   requests = logging.getLogger("requests") 
+   requests.addHandler(handler) 
+   
    # Load the models 
    from todo.models import db 
    from todo.models.todo import Todo 
@@ -38,5 +48,24 @@ def create_app(config_overrides=None):
    app.register_blueprint(api) 
 
    app.add_url_rule('/', 'index', lambda: app.send_static_file('index.html'))
+   @app.after_request 
+
+   def after_request(response): 
+      requests.info("Request finished") 
+      return response 
  
    return app
+
+
+class StructuredFormatter(watchtower.CloudWatchLogFormatter): 
+   def format(self, record): 
+      record.msg = { 
+         'timestamp': record.created, 
+         'location': record.name, 
+         'message': record.msg, 
+      } 
+      if has_request_context(): 
+         record.msg['request_id'] = request.environ.get('REQUEST_ID') 
+         record.msg['url'] = request.environ.get('PATH_INFO') 
+         record.msg['method'] = request.environ.get('REQUEST_METHOD') 
+      return super().format(record) 
